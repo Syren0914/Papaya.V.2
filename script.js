acb253e7bda813e6543c03d16dfebc102c6df470
Kira <78575847+Syren0914@users.noreply.github.com>
2024-02-29 14:35:48 -0800

    update

--- script.js
+++ script.js
@@ -38,36 +38,64 @@ document.addEventListener('DOMContentLoaded', function () {
   //Projects
   const projects = [
     {
-        image1: "https://images.unsplash.com/photo-1707343843437-caacff5cfa74?q=80&w=1975&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        image2 : "https://plus.unsplash.com/premium_photo-1666788168194-38222845bf00?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        image3: "https://images.unsplash.com/photo-1707343843598-39755549ac9a?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        type: "web",
-        transition: 'data-aos="fade-right"',
-        name:"",
-        description:"",
-        url:""
+        image1: "/assets/img/projects/web1.jpg",
+        image2 : "/assets/img/projects/Marketing1.jpg",
+        image3: "/assets/img/projects/Logo1.png",
+        image4:"",
+        image5:"",
+        image5:"",
+        image5:"",
+        image5:"",
+        type1: "web",
+        type2: "ads",
+        type3: "logo",
+        name1:"Saithong Para Thai",
+        name2:"ADS",
+        name3:"Logo Design",
+        description1:"Minimal Website design",
+        description2:"",
+        description3:"",
+        url1:"http://www.saithongparathai.com",
+        url2:"",
+        url3:""
     },
     {
-        image1: "https://images.unsplash.com/photo-1631778890260-2be2609eb9ec?q=80&w=2009&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        image2: "https://plus.unsplash.com/premium_photo-1661894459977-73692d71bd14?q=80&w=1769&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        image3: "https://images.unsplash.com/photo-1440613905118-99b921706b5c?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        type: "logo",
-        transition: 'data-aos="fade-down"',
-        name:"",
-        description:"",
-        url:""
-    },
-    {
-        image1: "https://images.unsplash.com/photo-1600517244515-c043b4c18415?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        image2 : "https://plus.unsplash.com/premium_photo-1666788168194-38222845bf00?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        image3: "https://images.unsplash.com/photo-1707343843598-39755549ac9a?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        type:"logo",
-        transition: 'data-aos="fade-left"',
-        name:"",
-        description:"",
-        url:""
+        image1: "/assets/img/projects/web2.jpg",
+        image2 : "/assets/img/projects/Marketing3.jpg",
+        image3: "/assets/img/projects/Logo3.png",
+        type1: "web",
+        type2: "ads",
+        type3: "logo",
+        name1:"",
+        name2:"ADS",
+        name3:"Logo Design",
+        description1:"Minimal Website design",
+        description2:"",
+        description3:"",
+        url1:"",
+        url2:"",
+        url3:""
 
     },
+    {
+        image1: "/assets/img/projects/web2.jpg",
+        image2: "/assets/img/projects/Marketing2.png",
+        image3: "/assets/img/projects/Logo2.png",
+        type1: "web",
+        type2: "ads",
+        type3: "logo",
+        name1:"Thai Love & Afterwork",
+        name2:"ADS",
+        name3:"Logo Design",
+        description1:"Minimal Website design",
+        description2:"",
+        description3:"",
+        url1:"http://www.thailove-afterwork.com",
+        url2:"",
+        url3:""
+    },
+    
+    
     
     // Add more project data as needed
 ];
@@ -77,32 +105,72 @@ function generateProjectItem(project) {
     return `
         <div class="column" >
         
-                <div class="photo ${project.type}" >
+                <div class="photo ${project.type1}" >
                     <img src="${project.image1} " alt="">
                     <div class="photo__content">
-                        <p class="photo__title">${project.name}</p>
-                        <p class="photo__description">${project.description}</p>
-                        <a target="_blank" href="${project.url}"><i class="fa-solid fa-up-right-from-square"></i></a>
+                        <p class="photo__title">${project.name1}</p>
+                        <p class="photo__description">${project.description1}</p>
+                        <a target="_blank" href="${project.url1}"><i class="fa-solid fa-up-right-from-square"></i></a>
                     </div>
                     
                 
 
                 </div>
-                <div class="photo ${project.type}" >
+                <div class="photo ${project.type2}" >
                     <img src="${project.image2}" alt="">
                     <div class="photo__content">
-                        <p class="photo__title">${project.name}</p>
-                        <p class="photo__description">${project.description}</p>
-                        <a target="_blank" href="${project.url}"><i class="fa-solid fa-up-right-from-square"></i></a>
+                        <p class="photo__title">${project.name2}</p>
+                        <p class="photo__description">${project.description2}</p>
+                        <a target="_blank" href="${project.url2}"><i class="fa-solid fa-up-right-from-square"></i></a>
                     </div>
                     
                 </div>
-                <div class="photo ${project.type}" >
+                <div class="photo ${project.type3}" >
                     <img src="${project.image3}" alt="">
                     <div class="photo__content">
-                        <p class="photo__title">${project.name}</p>
-                        <p class="photo__description">${project.description}</p>
-                        <a target="_blank" href="${project.url}"><i class="fa-solid fa-up-right-from-square"></i></a>
+                        <p class="photo__title">${project.name3}</p>
+                        <p class="photo__description">${project.description3}</p>
+                        <a target="_blank" href="${project.url3}"><i class="fa-solid fa-up-right-from-square"></i></a>
+                    </div>
+                </div>
+                <div class="photo ${project.type4}" >
+                    <img src="${project.image4}" alt="">
+                    <div class="photo__content">
+                        <p class="photo__title">${project.name4}</p>
+                        <p class="photo__description">${project.description4}</p>
+                        <a target="_blank" href="${project.url4}"><i class="fa-solid fa-up-right-from-square"></i></a>
+                    </div>
+                </div>
+                <div class="photo ${project.type5}" >
+                    <img src="${project.image5}" alt="">
+                    <div class="photo__content">
+                        <p class="photo__title">${project.name5}</p>
+                        <p class="photo__description">${project.description5}</p>
+                        <a target="_blank" href="${project.url5}"><i class="fa-solid fa-up-right-from-square"></i></a>
+                    </div>
+                </div>
+                <div class="photo ${project.type6}" >
+                    <img src="${project.image6}" alt="">
+                    <div class="photo__content">
+                        <p class="photo__title">${project.name6}</p>
+                        <p class="photo__description">${project.description6}</p>
+                        <a target="_blank" href="${project.url6}"><i class="fa-solid fa-up-right-from-square"></i></a>
+                    </div>
+                </div>
+                <div class="photo ${project.type7}" >
+                    <img src="${project.image7}" alt="">
+                    <div class="photo__content">
+                        <p class="photo__title">${project.name7}</p>
+                        <p class="photo__description">${project.description7}</p>
+                        <a target="_blank" href="${project.url7}"><i class="fa-solid fa-up-right-from-square"></i></a>
+                    </div>
+                </div>
+                <div class="photo ${project.type8}" >
+                    <img src="${project.image8}" alt="">
+                    <div class="photo__content">
+                        <p class="photo__title">${project.name8}</p>
+                        <p class="photo__description">${project.description8}</p>
+                        <a target="_blank" href="${project.url8}"><i class="fa-solid fa-up-right-from-square"></i></a>
                     </div>
                 </div>
         </div>
